f7746a3d684f8f62e83158bfb51213b586befd75
tranpham <pminhtran@gmail.com>
2017-05-11 17:50:46 -0700

    cHRM

---
+++ jsapi_embed.js
@@ -0,0 +1,34 @@
+// Initialize the viz variable
+var viz;
+window.onload= function() {
+// When the webpage has loaded, load the viz
+    var vizDiv = document.getElementById('myViz');
+//     var vizURL ="https://10az.online.tableau.com/t/f22/views/SalesDashboard_0/SalesDashBoard?:embed=y&:showShareOptions=true&:display_count=no&:showVizHome=no";
+     var vizURL ="https://us-east-1.online.tableau.com/t/projectx/views/Rocker/Headcount?:embed=y&:showShareOptions=true&:display_count=no&:showVizHome=no";
+     var options ={
+      width: '700px',
+      height: '700px',
+      hideToolbar: true,
+      hideTabs: true
+};
+ viz = new tableau.Viz(vizDiv, vizURL, options);
+    };
+
+// Switch the viz to the sheet specified
+function switchView(sheetName) {
+	var workbook =viz.getWorkbook();
+	workbook.activateSheetAsync(sheetName);
+
+}
+
+// Filter the specified dimension to the specified value(s)
+function show(filterName, values) {
+    var sheet =viz.getWorkbook().getActiveSheet();
+    sheet.applyFilterAsync(filterName,values,tableau.FilterUpdateType.REPLACE);
+}
+
+// Select the marks that have the specified value(s) for the specified dimension
+function selectMarks(filterName, values) {
+    var sheet =viz.getWorkbook().getActiveSheet();
+    sheet.applyFilterAsync(filterName,values,tableau.FilterUpdateType.REPLACE);
+}
