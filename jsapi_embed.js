9b13dedfa121015b9e10a505bbd72f7b3cd10380
Tran Pham <pminhtran@gmail.com>
2017-05-11 18:08:16 -0700

    Update jsapi_embed.js

--- jsapi_embed.js
+++ jsapi_embed.js
@@ -4,7 +4,7 @@ window.onload= function() {
 // When the webpage has loaded, load the viz
     var vizDiv = document.getElementById('myViz');
 //     var vizURL ="https://10az.online.tableau.com/t/f22/views/SalesDashboard_0/SalesDashBoard?:embed=y&:showShareOptions=true&:display_count=no&:showVizHome=no";
-     var vizURL ="https://us-east-1.online.tableau.com/t/projectx/views/Rocker/Headcount?:embed=y&:showShareOptions=true&:display_count=no&:showVizHome=no";
+     var vizURL ="https://us-east-1.online.tableau.com/t/projectx/views/RockerGroup/Genderchart?:embed=y&:showShareOptions=true&:display_count=no&:showVizHome=no";
      var options ={
       width: '700px',
       height: '700px',
